80d71f6e2015a519181c3ca6dda30dc070d8db9b
jrggementiza <jr.gementiza@bukas.ph>
2023-09-22 18:39:31 +0800

    Add mechanism for getting country based on query param

---
+++ country.py
@@ -0,0 +1,26 @@
+from typing import Tuple, List
+
+from pycountry import countries
+
+
+def get_country(country: str) -> Tuple[str, List[str | None]]:
+    result_country = None
+    result_options = []
+    # TODO: Gracefully handle LookupError for typos or spacing
+
+    # Allow standard country code input
+    country_code_result = countries.get(alpha_2=country)
+    if country_code_result:
+        result_country = country_code_result.common_name if hasattr(country_code_result, 'common_name') else country_code_result.name
+        return (result_country, result_options)
+
+    # Use Fuzzy Search if not Country Code
+    fuz_result = countries.search_fuzzy(country)
+    if len(fuz_result) > 1:
+        for res in fuz_result:
+            result_options.append(res.common_name if hasattr(res, 'common_name') else res.name)
+    else:
+        res = fuz_result[0]
+        result_country = res.common_name if hasattr(res, 'common_name') else res.name
+
+    return (result_country, result_options)
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 
+from country import get_country
 
 app = FastAPI()
 
 
 @app.get("/")
 def seasons(country: str, season: str):
+    # Intercept error if no country or season and standardize the response
+
+    matching_country, options = get_country(country)
+    if not matching_country:
+        return {
+            "message": f"Country {country} unclear. Is it any of the following?",
+            "options": options,
+        }
 
     return {
-        "country": country,
+        "country": matching_country,
         "season": season,
     }
