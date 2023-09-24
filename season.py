89c8b64c66444836a9bfd8a7203cc6daa262cf39
jrggementiza <jr.gementiza@bukas.ph>
2023-09-25 02:09:22 +0800

    Extract method as utility

--- season.py
+++ season.py
@@ -1,22 +1,10 @@
-import openai
-
-from config import OPENAI_API_KEY
-
-
-openai.api_key = OPENAI_API_KEY
+from utils import generate_response_from_prompt
 
 
 def get_matching_seasons(country: str):
     prompt = f"Given a {country}, what are that country's seasons. Just the season type, no need to specify months covered. Simplify the season naming. Like Dry, Rainy, Winter, Spring, Summer, Fall. Not Rainy Season, Dry Season, Winter Season. Make sure the seasons are correct for {country}. Expected output examples: 'winter, spring, summer, fall' or 'rainy, dry'"
 
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
-        max_tokens=50,
-        temperature=0.7
-    )
-
-    seasons_response = response.choices[0].text.strip()
+    seasons_response = generate_response_from_prompt(prompt)
     seasons_list = seasons_response.split(', ')
 
     return seasons_list
---
+++ utils.py
@@ -0,0 +1,17 @@
+import openai
+
+from config import OPENAI_API_KEY
+
+
+openai.api_key = OPENAI_API_KEY
+
+
+def generate_response_from_prompt(prompt, max_token=50) -> str:
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        max_tokens=max_token,
+        temperature=0.7
+    )
+
+    return response.choices[0].text.strip()
