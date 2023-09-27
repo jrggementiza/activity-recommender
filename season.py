e54addeea0d624db983ee326a5187ebad1d15c64
jrggementiza <jr.gementiza@bukas.ph>
2023-09-28 00:03:03 +0800

    Include alternate season names

--- season.py
+++ season.py
@@ -19,11 +19,11 @@ def _get_latitude_of_country(country: str) -> float:
 def _get_seasons_of_country(latitude: float) -> List[str]:
     MIN_MAX_LATITUDE_SEASONS = [
         (66.500000, 90.000000, ["Long Winter", "Short Summer"], "North Pole"),
-        (23.500000, 66.400000, ["Spring", "Summer", "Autumn", "Winter"], "Northern Hemisphere"),
-        (0.100000, 23.400000, ["Wet", "Dry"], "Tropic of Cancer"),
-        (0.000000, 0.000000, ["Wet", "Dry"], "Equator"),
-        (-23.400000, -0.100000, ["Wet", "Dry"], "Tropic of Capricorn"),
-        (-66.400000, -23.500000, ["Spring", "Summer", "Autumn", "Winter"], "Southern Hemisphere"),
+        (23.500000, 66.400000, ["Spring", "Summer", "Autumn", "Winter", "Fall"], "Northern Hemisphere"),
+        (0.100000, 23.400000, ["Wet", "Dry", "Rainy", "Summer"], "Tropic of Cancer"),
+        (0.000000, 0.000000, ["Wet", "Dry", "Rainy", "Summer"], "Equator"),
+        (-23.400000, -0.100000, ["Wet", "Dry", "Rainy", "Summer"], "Tropic of Capricorn"),
+        (-66.400000, -23.500000, ["Spring", "Summer", "Autumn", "Winter", "Fall"], "Southern Hemisphere"),
         (-90.000000, -66.500000, ["Long Winter", "Short Summer"], "South Pole")
     ]
     
